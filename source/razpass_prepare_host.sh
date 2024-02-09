50610e3fbf674f495cd11bc47d3278eb1bae0709
Jens Heine <binbash@gmx.net>
2024-02-09 23:42:54 +0100

    output cleaner

--- source/razpass_prepare_host.sh
+++ source/razpass_prepare_host.sh
@@ -9,27 +9,21 @@
 set -e
 ADHOC_WIFI_PASSWORD='razpassrazpass'
 
-echo -n "Set hostname to razpass... "
+echo "Set hostname to razpass... "
 hostname razpass
-echo "OK"
 
-echo -n "Clear /etc/motd... "
+echo "Clear /etc/motd... "
 echo > /etc/motd
-echo "OK"
 
-echo -n "Enable ssh server... "
+echo "Enable ssh server... "
 systemctl enable ssh
-echo "OK"
 
-echo -n "Enable ssh server... "
+echo "Enable ssh server... "
 systemctl enable ssh
-echo "OK"
 
-echo -n "Create ad-hoc wifi network with password ${ADHOC_WIFI_PASSWORD}... "
+echo "Create ad-hoc wifi network with password ${ADHOC_WIFI_PASSWORD}... "
 nmcli device wifi hotspot ifname wlan0 ssid razpass password ${ADHOC_WIFI_PASSWORD}
-echo "OK"
 
-echo -n "Enable ad-hoc network as default on boot... "
+echo "Enable ad-hoc network as default on boot... "
 nmcli connection modify Hotspot connection.autoconnect true
-echo "OK"
 
--- source/razpass_prepare_pi_user.sh
+++ source/razpass_prepare_pi_user.sh
@@ -9,42 +9,34 @@
 #
 set -e
 
-#echo -n "Set the password for the pi user: "
+#echo "Set the password for the pi user: "
 #passwd
 
-echo -n "Make sure that we are in the /home/pi folder... "
+echo "Make sure that we are in the /home/pi folder... "
 cd /home/pi
-echo "OK"
 
-echo -n "Create p program folder... "
+echo "Create p program folder... "
 mkdir p
 cd p
-echo "OK"
 
-echo -n "Download p from github... "
+echo "Download p from github... "
 wget https://github.com/binbash23/p/raw/master/dist/raspberry/p
-echo "OK"
 
-echo -n "Make p executeable... "
+echo "Make p executeable... "
 chmod +x p
-echo "OK"
 
-echo -n "Create and prepare .ssh folder in home dir... "
+echo "Create and prepare .ssh folder in home dir... "
 cd ~
 mkdir .ssh
 chmod 700 .ssh/
-echo "OK"
 
-echo -n "Make p executeable... "
+echo "Make p executeable... "
 chmod +x p
-echo "OK"
 
-
-echo -n "Make p executeable... "
+echo "Make p executeable... "
 chmod +x p
-echo "OK"
 
-echo -n "Prepare ~/.bash_profile to run p on login... "
+echo "Prepare ~/.bash_profile to run p on login... "
 cat >> ~/.bash_profile << EOF
 # when bash starts as a login shell: run p on login and exit the bash when p exists:
 cd p
@@ -52,5 +44,4 @@ echo "Please wait..."
 ./p
 exit
 EOF
-echo "OK"
 
