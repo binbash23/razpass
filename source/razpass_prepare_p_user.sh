7cd0df771f3783e5c4ce68bf0f4f2665ccaddddc
Jens Heine <binbash@gmx.net>
2024-02-10 21:01:37 +0100

    export path with ~/bin

--- source/razpass_prepare_p_user.sh
+++ source/razpass_prepare_p_user.sh
@@ -9,34 +9,32 @@
 #
 set -e
 
-echo "Make sure that we are in the home folder... "
-cd ~
+echo "Create ~/bin folder..."
+mkdir ~/bin
+
+echo "Copy useful bash scripts into ~/bin..."
+cp -v home_p_bin/* ~/bin
 
 echo "Create p program folder... "
-mkdir p
-cd p
+mkdir ~/p
+cd ~/p
 
 echo "Download p from github... "
 wget https://github.com/binbash23/p/raw/master/dist/raspberry/p
 
 echo "Make p executeable... "
-chmod +x p
+chmod +x ./p
 
 echo "Create and prepare .ssh folder in home dir... "
 cd ~
 mkdir .ssh
 chmod 700 .ssh/
 
-echo "Make p executeable... "
-chmod +x p
-
-echo "Make p executeable... "
-chmod +x p
-
 echo "Prepare ~/.bash_profile to run p on login... "
-cat >> ~/.bash_profile << EOF
+cat > ~/.bash_profile << EOF
+export PATH="~/bin:$PATH"
 # when bash starts as a login shell: run p on login and exit the bash when p exists:
-cd p
+cd ~/p
 echo "Please wait..."
 ./p
 exit
